aa970c0ed40521299ea5fdd3618cbd32969b906c
12usama <usamaalam3999@gmail.com>
2023-10-14 02:17:20 +0600

    last edit

--- src/Layouts/Home/Home.jsx
+++ src/Layouts/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="max-w-7xl mx-auto">
             <div className="h-[60vh] lg:h-[80vh] bg-cover  bg-[url('https://i.ibb.co/bsmXHmz/Enjoy-the-Time-Enjoy-your-life.png')]">
                 <div className="grid justify-center h-full">
-                    <h1 className="text-6xl font-bold text-white mt-5">Stay With Entseeker</h1>
+                    <h1 className=" text-2xl lg:text-6xl font-bold text-white mt-5">Stay With Entseeker</h1>
                 </div>
             </div>
             <div className="grid mt-8">
